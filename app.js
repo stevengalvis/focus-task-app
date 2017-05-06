29846eccd133231a8f94bacd2ea041a2942ed366
stevengalvis <steven.galvis@gmail.com>
2017-05-06 16:30:46 -0400

    added event listener for delete button and function for get api data

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 
-function renderTask(task) {
-console.log(task);
+var GIPHY_URL = 'http://api.giphy.com/v1/gifs/search';
+
+function getDataFromAPI(searchTerm){
+    var query = {
+        limit: 1,
+        q: searchTerm,
+        api_key: 'dc6zaTOxFJmzC'
+    }
+    $.getJSON(GIPHY_URL, query, function(data){
+        console.log(data);
+        // $('.task-result')
+    });
 }
 
+function renderTask(task) {
+    var taskElement = '<button type="button" class = "complete-button" name="button">Done</button>' + '<span>' + task + '</span>'  +
+    '<button type ="button" class ="reset-button" id="reset" name="reset">Delete</button>';
+    $('.task-result').html(taskElement);
+    hideAddInput();
+    waitforDelete();
+}
+
+function hideAddInput(){
+    $('.task-form').addClass('visuallyhidden');
+}
+
+function waitforDelete(){
+    $('.task-result').on('click', '.reset-button', function(e){
+    alert('hi');
+    console.log('ok');
+    });
+}
 
 
 
@@ -10,11 +38,13 @@ console.log(task);
 
 
 //event listeners
-function watchAddTask(){
-    $('.task-form').on('submit',function(e){
+
+
+
+    $('.task-form').on('submit', function(e){
         e.preventDefault();
         renderTask($('.text-input').val());
     });
-}
 
-$(function(){watchAddTask();})
+
+$(function(){});
