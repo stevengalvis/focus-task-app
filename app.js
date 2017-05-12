4f7d8f43e493c9def5a8e2a8b0a97dd1da86ac6d
stevengalvis <steven.galvis@gmail.com>
2017-05-12 05:53:38 -0400

    adding pop ups

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
+'use strict';
 var GIPHY_URL = 'http://api.giphy.com/v1/gifs/random';
+var countdown = void 0;
+var timerDisplay = document.querySelector('.display-time-left');
+var buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds){
+    //clear any existing timerDisplay
+    clearInterval(countdown);
+    var now = Date.now();
+    var then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(function(){
+        var secondsLeft = Math.round((then - Date.now()) / 1000);
+        //check if we should stop it
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    },1000);
+}
+
+function displayTimeLeft(seconds){
+    var minutes = Math.floor(seconds / 60);
+    var remainderSeconds = seconds % 60;
+    var display = minutes + ':' + (remainderSeconds < 10 ? '0': '') + remainderSeconds;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+
+
+function startTimer(){
+    var seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(function(button){
+    return button.addEventListener('click', startTimer);
+});
+
+document.customForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    var mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
+
 
 function getDataFromAPI(searchTerm){
     var query = {
@@ -46,6 +96,7 @@ function waitforDelete(){
 
 function waitForDone(){
     $('.task-result').on('click','.done-button',function(e){
+        swal("Good job!", "You completed the task!", "success");
         getDataFromAPI('success');
         $('.task-form').removeClass('visually-hidden');
         $('.task-form').trigger('reset');
@@ -69,4 +120,5 @@ $(function(){
     watchForSubmit();
     waitforDelete();
     waitForDone();
+
 });
