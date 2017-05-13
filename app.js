cb7e9c4d974b3df11d613fed91cf4140fd737b20
stevengalvis <steven.galvis@gmail.com>
2017-05-13 15:32:42 -0400

    added blinking animation and hover animation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var timerDisplay = document.querySelector('.display-time-left');
 
 function timer(seconds){
     //clear any existing timerDisplay
+    $('.display-time-left').removeClass('blink blink-infinite');
     clearInterval(countdown);
     var now = Date.now();
     var then = now + seconds * 1000;
@@ -17,6 +18,8 @@ function timer(seconds){
         if(secondsLeft < 0){
             clearInterval(countdown);
             swal("All Done!", "Time for a break", "success");
+            $('.timer-header').html('Press timer to restart');
+            $('.display-time-left').addClass('blink blink-infinite');
             return;
         }
         displayTimeLeft(secondsLeft);
@@ -51,11 +54,11 @@ function getDataFromAPI(searchTerm){
 }
 
 function renderTask(task) {
-    var taskElement = '<button type="button" class = "done-button" name="button"><i class="fa fa-check fa-3x" aria-hidden="true"></i></button>' + '<span class = "task-name">' + task + '</span>'  +
-    '<button type ="button" class ="reset-button" id="reset" name="reset"><i class="fa fa-trash-o fa-3x" aria-hidden="true"></i></button>'+ '<button type = "button" class = "start-timer">' +
+    var taskElement = '<button type="button" class = "done-button" name="button" title = "mark completed"><i class="fa fa-check fa-3x" aria-hidden="true"></i></button>' + '<span class = "task-name">' + task + '</span>'  +
+    '<button type ="button" class ="reset-button" id="reset" name="reset" title = "delete item"><i class="fa fa-trash-o fa-3x" aria-hidden="true"></i></button>'+ '<button type = "button" class = "start-timer" title = "reset time">' +
 '<i class="fa fa-clock-o fa-3x" aria-hidden="true"></i></button>';
     $('.task-result').html(taskElement);
-
+    $('.timer-header').removeClass('visually-hidden');
     hideAddInput();
 }
 
@@ -68,6 +71,7 @@ function hideAddInput(){
 //event listenerss
 function waitforDelete(){
     $('.task-result').on('click', '.reset-button', function(e){
+        $('.timer-header').addClass('visually-hidden');
         getDataFromAPI('fail');
         $('.task-form').removeClass('visually-hidden');
         $('.task-form').trigger('reset');
@@ -80,6 +84,7 @@ function waitforDelete(){
 
 function waitForDone(){
     $('.task-result').on('click','.done-button',function(e){
+        $('.timer-header').addClass('visually-hidden');
         swal("Good job!", "You completed the task!", "success");
         getDataFromAPI('success');
         $('.task-form').removeClass('visually-hidden');
@@ -104,6 +109,11 @@ function waitForSetTimer(){
         setTimer();
     });
 }
+
+$('.display-time-left').on('click', function(e){
+    event.preventDefault;
+    setTimer();
+})
 
 function setTimer(){
     swal({
